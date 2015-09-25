14268664264da1c4780f0151129cc3dc5bb87cd8
Richard George <richard@phase.org>
2015-09-25 14:23:46 +0100

    pass ted-http tests

--- exercises/tdd-http/exercise.js
+++ exercises/tdd-http/exercise.js
@@ -1,25 +1,52 @@
-
 var express = require("express");
 
 var game = require("./game");
 
 
 // TODO create server
+var app = express();
 
 // TODO export so we can test it
+module.exports = app;
 
 // TODO need to parse bodies
-
-
-// TODO validate 'name' is present in body
+app.use(require('body-parser').json());
 
 // TODO accept posts to /players
+app.post('/players', function(req, res) {
 
+  //res.type('json');
+
+// TODO validate 'name' is present in body
+  if (req.body.name) {
+    //game.join(req.body, cb);
 // TODO pass an object with following keys/values
 // - name: name sent in post body
 // validate the name is present
+    game.join({name: req.body.name}, cb);
+  } else {
+    res.status(400).send(
+      {error: 'missing name'}
+    );
+  }
 
 // TODO handle errors from game.join
+  function cb(error, data) {
+    if (error) {
+      res.status(500).send({error: 'internal error'});
+    } else {
+      res.send(data);
+    }
+  }
+});
 
 
+if (require.main === module) {
+  main();
+}
 
+function main() {
+  app.listen(3000, function() {
+    console.log('listening');
+  });
+}
